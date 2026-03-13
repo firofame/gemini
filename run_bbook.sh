46f51a4889828267f39bcd27c9cbff091650f0e9
Firoz Ahmed <firofame@users.noreply.github.com>
2026-03-13 15:50:07 +0530

    .

--- run_bbook.sh
+++ run_bbook.sh
@@ -6,11 +6,7 @@ if [ -z "$1" ]; then
 fi
 
 bbook_maker \
-  --model openai \
-  --model_list "gemini-3.1-pro-low" \
-  --openai_key "sk-8e2af9daeacb423e9ac505c1faa6c976" \
-  --api_base "http://127.0.0.1:8045/v1" \
-  --batch_size 200 \
+  --model google \
   --language ml \
   --single_translate \
   --prompt "prompt.txt" \
